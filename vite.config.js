a9b2c17337a328e0ec42db912e06c5aeed0d00ae
sharif-minhaz <minhaz.rbs@gmail.com>
2025-07-23 11:41:58 +0600

    fix: eslint issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
-import eslint from "vite-plugin-eslint";
+// import eslint from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), eslint()],
+	plugins: [
+		react(),
+		// eslint({
+		// 	include: [`${path.resolve(path.dirname(), "")}/**/*.js`],
+		// }),
+	],
 	resolve: {
 		alias: {
 			"@": "/src",
