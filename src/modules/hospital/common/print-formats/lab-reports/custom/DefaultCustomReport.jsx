9e664da4538055a88a96152a344295916b2392b4
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2025-12-11 09:52:47 +0600

    Update report format

--- src/modules/hospital/common/print-formats/lab-reports/custom/DefaultCustomReport.jsx
+++ src/modules/hospital/common/print-formats/lab-reports/custom/DefaultCustomReport.jsx
@@ -11,7 +11,7 @@ const DefaultCustomReport = forwardRef(({reportData,report}) => {
 	console.log(mainAreaHeight)
 	return (
 		<Box>
-			<Box mb="md" p={'xl'} pt={'md'} fz={'md'} h={600}>
+			<Box mb="md" p={'xl'} pt={'md'} fz={'md'}>
 				<div dangerouslySetInnerHTML={{__html:getValue(reportData?.findings)}}/>
 				{report?.comment && (
 					<Box p="md" pt={0}>
@@ -24,23 +24,6 @@ const DefaultCustomReport = forwardRef(({reportData,report}) => {
 					</Box>
 				)}
 			</Box>
-			{/* =============== Additional Information Section ================ */}
-			<Box p="md" pt={0} pb={0}>
-				<Grid columns={12} gutter="xs">
-					<Grid.Col span={8}/>
-					<Grid.Col span={4}>
-						<Box>
-							<Box h={40} ta="center">
-								{/*{renderImagePreview([], patientInfo?.signature_path)}*/}
-							</Box>
-							<Text fw="bold" size="xs" mb="sm" ta="center">
-								{report?.assign_doctor_name}
-							</Text>
-						</Box>
-					</Grid.Col>
-				</Grid>
-			</Box>
-			{/* =============== Doctor Information and Signature ================ */}
 		</Box>
 	);
 });
