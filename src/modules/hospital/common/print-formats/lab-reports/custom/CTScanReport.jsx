8137d815029e051542499aceb47cf526946ba8da
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2025-12-17 15:00:42 +0600

    Update report format

--- src/modules/hospital/common/print-formats/lab-reports/custom/CTScanReport.jsx
+++ src/modules/hospital/common/print-formats/lab-reports/custom/CTScanReport.jsx
@@ -13,14 +13,14 @@ const CTScanReport = forwardRef(({reportData,report}) => {
 		<Box>
 			<Box mb="md" p={'xl'} pt={'xs'}  fz={'md'} h={600}>
 				<Grid>
-					<Grid.Col span={3} fw={'600'}>Technique</Grid.Col>
+					<Grid.Col span={4} fw={'600'}>Technique</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.technique)}}/></Grid.Col>
 				</Grid>
 				<Grid>
 					<Grid.Col span={12} fw={'600'}>Findings</Grid.Col>
 				</Grid>
 				<Grid>
-					<Grid.Col span={3}>Parenchyma</Grid.Col>
+					<Grid.Col span={4}>Parenchyma</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.parenchyma)}}/></Grid.Col>
 				</Grid>
 				<Grid>
@@ -28,36 +28,36 @@ const CTScanReport = forwardRef(({reportData,report}) => {
 				</Grid>
 				<Grid>
 					<Grid.Col span={1}/>
-					<Grid.Col span={2} >a. Vessels</Grid.Col>
+					<Grid.Col span={3} >a. Vessels</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.mediastinum_vessels)}}/></Grid.Col>
 				</Grid>
 				<Grid>
 					<Grid.Col span={1}/>
-					<Grid.Col span={2} >b. Trachea-</Grid.Col>
+					<Grid.Col span={3} >b. Trachea-</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.mediastinum_trachea)}}/></Grid.Col>
 				</Grid>
 				<Grid>
 					<Grid.Col span={1}/>
-					<Grid.Col span={2} >c. Oesophagus-</Grid.Col>
+					<Grid.Col span={3} >c. Oesophagus-</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.mediastinum_oesophagus)}}/></Grid.Col>
 				</Grid>
 				<Grid>
 					<Grid.Col span={1}/>
-					<Grid.Col span={2} >d. Thymus-</Grid.Col>
+					<Grid.Col span={3} >d. Thymus-</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.mediastinum_thymus)}}/></Grid.Col>
 				</Grid>
 				<Grid>
 					<Grid.Col span={1}/>
-					<Grid.Col span={2} >d. Lymph Nodes-</Grid.Col>
+					<Grid.Col span={3} >d. Lymph Nodes-</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.mediastinum_lymph_nodes)}}/></Grid.Col>
 				</Grid>
 				<Grid>
-					<Grid.Col span={3}>Heart</Grid.Col>
+					<Grid.Col span={4}>Heart</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.heart)}}/></Grid.Col>
 				</Grid>
 
 				<Grid>
-					<Grid.Col span={3}>Pleura</Grid.Col>
+					<Grid.Col span={4}>Pleura</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.pleura)}}/></Grid.Col>
 				</Grid>
 				<Grid>
@@ -65,13 +65,12 @@ const CTScanReport = forwardRef(({reportData,report}) => {
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.bones)}}/></Grid.Col>
 				</Grid>
 				<Grid>
-					<Grid.Col span={3}>After I/V Contrast</Grid.Col>
+					<Grid.Col span={4}>After I/V Contrast</Grid.Col>
 					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.after_iv_contrast)}}/></Grid.Col>
 				</Grid>
 				<Grid>
-					<Grid.Col span={3}  fw={'600'}>Impression</Grid.Col>
-					<Grid.Col span={9}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.impression)}}/></Grid.Col>
-
+					<Grid.Col span={4}  fw={'600'}>Impression</Grid.Col>
+					<Grid.Col span={9} fw={'600'}><div dangerouslySetInnerHTML={{__html:getValue(reportData?.impression)}}/></Grid.Col>
 				</Grid>
 			</Box>
 			{/* =============== Doctor Information and Signature ================ */}
