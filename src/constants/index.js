492f616f369df8f884d120cda86d8f79a1a740e7
sharif-minhaz <minhaz.rbs@gmail.com>
2025-12-17 16:17:25 +0600

    refactor: replace import.meta.env with API_GATEWAY_URL for API_BASE_URL definition

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 import { API_GATEWAY_URL } from "../config.js";
 
-export const API_BASE_URL = import.meta.env.VITE_API_GATEWAY_URL || "http://localhost:8080";
+export const API_BASE_URL = API_GATEWAY_URL || "http://localhost:8080";
 export const API_KEY = import.meta.env.VITE_API_KEY;
 export const SUCCESS_NOTIFICATION_COLOR = "#008080";
 export const ERROR_NOTIFICATION_COLOR = "#FA5252";
