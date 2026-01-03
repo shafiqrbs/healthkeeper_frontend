8e1d9ce6fcd851d53e079f6ae3caf0961061d1b5
sharif-minhaz <minhaz.rbs@gmail.com>
2026-01-03 22:14:09 +0600

    refactor: clean up BillingActions and BillingSummary components for improved readability and consistency

--- src/modules/hospital/common/BillingActions.jsx
+++ src/modules/hospital/common/BillingActions.jsx
@@ -1,38 +1,27 @@
-import { ActionIcon, Box, Button, Flex, Text } from "@mantine/core";
-// import PaymentMethodsCarousel from "./PaymentMethodsCarousel";
-import { PAYMENT_METHODS } from "@/constants/paymentMethods";
-import { useForm } from "@mantine/form";
+import { Box, Button, Flex, Text } from "@mantine/core";
 import { useTranslation } from "react-i18next";
-import InputNumberForm from "@/common/components/form-builders/InputNumberForm";
-import { IconArrowsSplit2 } from "@tabler/icons-react";
-import {getDataWithoutStore} from "@/services/apiService";
-import {HOSPITAL_DATA_ROUTES} from "@/constants/routes";
-import {useParams} from "react-router-dom";
-import {useEffect, useRef, useState} from "react";
-import {useReactToPrint} from "react-to-print";
+import { getDataWithoutStore } from "@/services/apiService";
+import { HOSPITAL_DATA_ROUTES } from "@/constants/routes";
+import { useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useReactToPrint } from "react-to-print";
 import RefundFormInvestigationBN from "@hospital-components/print-formats/refund/RefundFormInvestigationBN";
 import RefundFromBedBn from "@hospital-components/print-formats/refund/RefundFormBedBN";
 import IPDInvoicePosBn from "@hospital-components/print-formats/ipd/IPDInvoicePosBN";
 
-export default function BillingActions({entity}) {
+export default function BillingActions({ entity }) {
 	const { t } = useTranslation();
 	const { id } = useParams();
 	const invoicePrintRef = useRef(null);
 	const [invoicePrintData, setInvoicePrintData] = useState(null);
-	const [investigationRecords, setInvestigationRecords] = useState([]);
 	const invoicePrint = useReactToPrint({ content: () => invoicePrintRef.current });
-	const [invoiceEntity,setInvoiceEntity] = useState({});
-	const form = useForm({
-		initialValues: {
-			paymentMethod: PAYMENT_METHODS[0],
-		},
-	});
+	const [invoiceEntity, setInvoiceEntity] = useState({});
 
 	// const selectPaymentMethod = (method) => {
 	// 	form.setFieldValue("paymentMethod", method);
 	// };
 
-	const  handlePrescriptionPosSubmit = async () => {
+	const handlePrescriptionPosSubmit = async () => {
 		const res = await getDataWithoutStore({
 			url: `${HOSPITAL_DATA_ROUTES.API_ROUTES.FINAL_BILLING.VIEW}/${id}/final-bill-process`,
 		});
@@ -40,7 +29,7 @@ export default function BillingActions({entity}) {
 		setInvoicePrintData(res);
 	};
 
-	const  handlePrescriptionPrint = async () => {
+	const handlePrescriptionPrint = async () => {
 		const res = await getDataWithoutStore({
 			url: `${HOSPITAL_DATA_ROUTES.API_ROUTES.FINAL_BILLING.VIEW}/${id}/final-bill-process`,
 		});
@@ -48,59 +37,92 @@ export default function BillingActions({entity}) {
 	};
 
 	useEffect(() => {
-		if(invoicePrintData){
+		if (invoicePrintData) {
 			invoicePrint();
 		}
 	}, [invoicePrintData]);
 
 	useEffect(() => {
-		if(entity){
+		if (entity) {
 			setInvoiceEntity(entity);
-			console.log('ok');
 		}
 	}, [entity]);
 
+	const receive = invoiceEntity?.remaining_day * invoiceEntity?.room_price;
 
-	const receive = invoiceEntity?.remaining_day * invoiceEntity?.room_price
-	console.log(invoiceEntity);
 	return (
 		<Box p="xs" mt="xs" bg="var(--theme-tertiary-color-0)">
 			<Flex justify="space-between" align="center">
-
 				<Flex gap="xs" align="center">
 					<Box bg="var(--mantine-color-white)" px="xs" py="les" className="borderRadiusAll">
 						<Text fz="sm" fw={600} style={{ textWrap: "nowrap" }}>
-							{t("Amount")} ৳ {receive}
+							{t("Amount")} ৳ {receive || 0}
 						</Text>
 					</Box>
 				</Flex>
 				<Flex fz="sm" align="center" gap="xs">
 					<Button.Group>
-						{invoiceEntity?.process === 'paid'  && (
-							<Button  w="100%" bg="var(--theme-primary-color-6)" type="button" onClick={handlePrescriptionPrint}>Print</Button>
+						{invoiceEntity?.process === "paid" && (
+							<Button
+								w="100%"
+								bg="var(--theme-primary-color-6)"
+								type="button"
+								onClick={handlePrescriptionPrint}
+							>
+								Print
+							</Button>
 						)}
-						{invoiceEntity?.process === 'refund' && (
-							<Button  w="100%" bg="var(--theme-warn-color-6)" type="button" onClick={handlePrescriptionPrint}>Print</Button>
+						{invoiceEntity?.process === "refund" && (
+							<Button
+								w="100%"
+								bg="var(--theme-warn-color-6)"
+								type="button"
+								onClick={handlePrescriptionPrint}
+							>
+								Print
+							</Button>
 						)}
 					</Button.Group>
 					<Button.Group>
-						{invoiceEntity?.process === 'admitted' && receive === 0 && (
-							<Button  w="100%" bg="var(--theme-primary-color-6)" type="button" onClick={handlePrescriptionPosSubmit}>SETTLED</Button>
+						{invoiceEntity?.process === "admitted" && receive === 0 && (
+							<Button
+								w="100%"
+								bg="var(--theme-primary-color-6)"
+								type="button"
+								onClick={handlePrescriptionPosSubmit}
+							>
+								SETTLED
+							</Button>
 						)}
-						{invoiceEntity?.process === 'admitted' && receive > 0 && (
-							<Button  w="100%" bg="var(--theme-secondary-color-6)" type="button" onClick={handlePrescriptionPosSubmit}>RECEIVE</Button>
+						{invoiceEntity?.process === "admitted" && receive > 0 && (
+							<Button
+								w="100%"
+								bg="var(--theme-secondary-color-6)"
+								type="button"
+								onClick={handlePrescriptionPosSubmit}
+							>
+								RECEIVE
+							</Button>
 						)}
-						{(invoiceEntity?.process === 'admitted' || invoiceEntity?.process === 'refund') && receive < 0 && (
-							<Button  w="100%" bg="var(--theme-warn-color-6)" type="button" onClick={handlePrescriptionPosSubmit}>REFUND</Button>
-						)}
+						{(invoiceEntity?.process === "admitted" || invoiceEntity?.process === "refund") &&
+							receive < 0 && (
+								<Button
+									w="100%"
+									bg="var(--theme-warn-color-6)"
+									type="button"
+									onClick={handlePrescriptionPosSubmit}
+								>
+									REFUND
+								</Button>
+							)}
 					</Button.Group>
 				</Flex>
 			</Flex>
 
-			{invoiceEntity?.process === 'paid' && receive === 0 && (
+			{invoiceEntity?.process === "paid" && receive === 0 && (
 				<IPDInvoicePosBn data={invoicePrintData?.data} ref={invoicePrintRef} />
 			)}
-			{(invoiceEntity?.process === 'refund') && receive < 0 && (
+			{invoiceEntity?.process === "refund" && receive < 0 && (
 				<RefundFromBedBn data={invoicePrintData?.data} ref={invoicePrintRef} />
 			)}
 		</Box>
--- src/modules/hospital/common/BillingSummary.jsx
+++ src/modules/hospital/common/BillingSummary.jsx
@@ -1,25 +1,25 @@
-import {Box, Divider, Flex, ScrollArea, Text} from "@mantine/core";
+import { Box, Divider, Flex, Text } from "@mantine/core";
 
 export default function BillingSummary({ entity, data }) {
-	const receive = entity?.remaining_day*entity?.room_price
+	const receive = entity?.remaining_day * entity?.room_price;
 	return (
 		<Box p="xs" bg="var(--theme-primary-color-0)">
 			<Box bg="var(--mantine-color-white)" p="xs">
 				<Flex justify="space-between">
-					<Text>Admission- ({entity.room_admission_day})Days: </Text>
-					<Text>৳ {entity.room_admission_day * entity?.room_price}</Text>
+					<Text>Admission- ({entity.room_admission_day || 0})Days: </Text>
+					<Text>৳ {entity.room_admission_day * entity?.room_price || 0}</Text>
 				</Flex>
 				<Flex justify="space-between">
-					<Text>Payment - ({entity.room_consume_day})Days: </Text>
-					<Text>৳ {entity.room_consume_day  * entity?.room_price}</Text>
+					<Text>Payment - ({entity.room_consume_day || 0})Days: </Text>
+					<Text>৳ {entity.room_consume_day * entity?.room_price || 0}</Text>
 				</Flex>
 				<Flex justify="space-between">
-					<Text>Payable -({entity.remaining_day})Days: </Text>
-					<Text>৳ {entity.remaining_day  * entity?.room_price}</Text>
+					<Text>Payable -({entity.remaining_day || 0})Days: </Text>
+					<Text>৳ {entity.remaining_day * entity?.room_price || 0}</Text>
 				</Flex>
 				<Divider my="xs" />
 				<Flex justify="space-between">
-					<Text>{ receive >= 0 ? "Receivable" : "Refund" } </Text>
+					<Text>{receive >= 0 ? "Receivable" : "Refund"} </Text>
 					<Text>৳ {receive}</Text>
 				</Flex>
 			</Box>
