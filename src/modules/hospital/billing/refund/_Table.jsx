e4b07876e61e0f5b5188b8d9c9be931656afc31d
sharif-minhaz <minhaz.rbs@gmail.com>
2026-01-11 08:41:42 +0600

    refactor: implement pagination in refund table with updated per-page settings and improved layout for better user experience

--- src/modules/hospital/billing/refund/_Table.jsx
+++ src/modules/hospital/billing/refund/_Table.jsx
@@ -5,12 +5,13 @@ import { HOSPITAL_DATA_ROUTES } from "@/constants/routes";
 import { useState } from "react";
 import { MODULES_CORE } from "@/constants";
 import { formatDate } from "@utils/index";
-import useInfiniteTableScroll from "@hooks/useInfiniteTableScroll";
 import { useSelector } from "react-redux";
 import CustomDivider from "@components/core-component/CustomDivider";
+import usePagination from "@hooks/usePagination";
+import PaginationBottomSection from "@components/tables/PaginationBottomSection";
 
 const module = MODULES_CORE.REFUND;
-const PER_PAGE = 500;
+const PER_PAGE = 25;
 
 export default function _Table({ patient_mode }) {
 	const { id } = useParams();
@@ -24,7 +25,7 @@ export default function _Table({ patient_mode }) {
 		navigate(`${HOSPITAL_DATA_ROUTES.NAVIGATION_LINKS.REFUND.VIEW}/${id}`);
 	};
 
-	const { records, fetching } = useInfiniteTableScroll({
+	const { records, fetching, handlePageChange, page, total, totalPages, perPage } = usePagination({
 		module,
 		fetchUrl: HOSPITAL_DATA_ROUTES.API_ROUTES.REFUND.INDEX,
 		perPage: PER_PAGE,
@@ -43,15 +44,33 @@ export default function _Table({ patient_mode }) {
 
 	return (
 		<Box>
-			<Flex gap="sm" p="les" c="white" bg="var(--theme-primary-color-6)" mt="3xs">
-				<Text ta="center" fz="sm" fw={500}>
-					S/N
-				</Text>
-				<Text ta="center" fz="sm" fw={500}>
-					Patient Name
-				</Text>
+			<Flex
+				justify="space-between"
+				align="center"
+				gap="sm"
+				p="les"
+				c="white"
+				bg="var(--theme-primary-color-6)"
+				mt="3xs"
+			>
+				<Flex align="center" justify="space-between" gap="sm">
+					<Text ta="center" fz="sm" fw={500}>
+						S/N
+					</Text>
+					<Text ta="center" fz="sm" fw={500}>
+						Patient Name
+					</Text>
+				</Flex>
+				<PaginationBottomSection
+					isCompact={true}
+					perPage={perPage}
+					page={page}
+					totalPages={totalPages}
+					handlePageChange={handlePageChange}
+					total={total}
+				/>
 			</Flex>
-			<ScrollArea bg="var(--mantine-color-white)" h={mainAreaHeight - 164} scrollbars="y" px="3xs">
+			<ScrollArea pos="relative" bg="var(--mantine-color-white)" h={mainAreaHeight - 216} scrollbars="y" px="3xs">
 				<LoadingOverlay visible={fetching} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
 				{records?.map((item) => (
 					<Grid
@@ -67,7 +86,11 @@ export default function _Table({ patient_mode }) {
 						px="xs"
 						gutter="xs"
 					>
-						<Grid.Col span={12}><Text fz="sm" fw={'600'}>{item.name}</Text></Grid.Col>
+						<Grid.Col span={12}>
+							<Text fz="sm" fw={"600"}>
+								{item.name}
+							</Text>
+						</Grid.Col>
 						<CustomDivider />
 						<Grid.Col span={6}>
 							<Flex align="center" gap="3xs">
@@ -83,7 +106,6 @@ export default function _Table({ patient_mode }) {
 							<Flex align="center" gap="3xs">
 								<IconUser size={16} stroke={1.5} />
 								<Text fz="sm">{item.mobile}</Text>
-
 							</Flex>
 						</Grid.Col>
 						<Grid.Col span={6}>
@@ -108,6 +130,13 @@ export default function _Table({ patient_mode }) {
 					</Grid>
 				))}
 			</ScrollArea>
+			<PaginationBottomSection
+				perPage={perPage}
+				page={page}
+				totalPages={totalPages}
+				handlePageChange={handlePageChange}
+				total={total}
+			/>
 		</Box>
 	);
 }
